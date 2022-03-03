d61ef4c83e693f943bb70a48e0c4223dab674d72
Oscar Minjarez <hioscarminjarezdev@gmail.com>
2022-03-03 16:17:37 -0700

    Simulate database with JSON file

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
-$contacts = [
-  ["name" => "Pepe", "phone" => "9933993399"],
-  ["name" => "Antonio", "phone" => "9933994455"],
-  ["name" => "Nate", "phone" => "1313121225"],
-  ["name" => "Rodrigo", "phone" => "3434343434"],
-  ["name" => "Marcos", "phone" => "1234556789"]
-];
+if (file_exists("contacts.json")) {
+  $contacts = json_decode(file_get_contents("contacts.json"), true);
+} else {
+  $contacts = [];
+}
 
 ?>
 
@@ -61,7 +59,7 @@ $contacts = [
             <a class="nav-link" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="./add.html">Add Contact</a>
+            <a class="nav-link" href="./add.php">Add Contact</a>
           </li>
         </ul>
       </div>
@@ -72,18 +70,26 @@ $contacts = [
     <div class="container pt-4 p-3">
       <div class="row">
 
-        <?php foreach($contacts as $contact) { ?>
+        <?php if (count($contacts) == 0): ?>
+          <div class="col-md-4 mx-auto">
+            <div class="card card-body text-center">
+              <p>No contacts saved yet</p>
+              <a href="add.php">Add One!</a>
+            </div>
+          </div>
+        <?php endif ?>
+        <?php foreach($contacts as $contact) : ?>
           <div class="col-md-4 mb-3">
             <div class="card text-center">
               <div class="card-body">
                 <h3 class="card-title text-capitalize"><?= $contact["name"] ?></h3>
-                <p class="m-2"><?= $contact["phone"] ?></p>
+                <p class="m-2"><?= $contact["phone_number"] ?></p>
                 <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
                 <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
               </div>
             </div>
           </div>
-        <?php } ?>
+        <?php endforeach ?>
 
       </div>
     </div>
