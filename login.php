0f2c17c334876c38d9eaf8710b1d651b8721e3aa
OscarMinjarez <oscarminjarez@outlook.com>
2022-10-20 18:25:35 -0700

    Login and logout

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
   $error = null;
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"]) || empty($_POST["email"]) || empty($_POST["password"])) {
+    if (empty($_POST["email"]) || empty($_POST["password"])) {
       $error = "Please fill the all fields";
     } else if (!str_contains($_POST["email"], "@")) {
       $error = "Email format is incorret";
@@ -13,26 +13,22 @@
       $statement->bindParam(":email", $_POST["email"]);
       $statement->execute();
 
-      if ($statement->rowCount() > 0) {
-        $error = "This email is taken.";
+      if ($statement->rowCount() == 0) {
+        $error = "Invalid credentials.";
       } else {
-        $conn
-          ->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)")
-          ->execute([
-            ":name" => $_POST["name"],
-            ":email" => $_POST["email"],
-            ":password" => password_hash($_POST["password"], PASSWORD_BCRYPT)
-          ]);
-
-        $statement = $conn->prepare("SELECT * FROM users WHERE email = :email");
-        $statement->bindParam(":email", $_POST["email"]);
-        $statement->execute();
         $user = $statement->fetch(PDO::FETCH_ASSOC);
+        
+        if (!password_verify($_POST["password"], $user["password"])) {
+          $error = "Invalid credentials.";
+        } else {
+          session_start();
 
-        session_start();
-        $_SESSION["user"] = $user;
+          unset($user["password"]);
 
-        header("Location: home.php");
+          $_SESSION["user"] = $user;
+
+          header("Location: home.php");
+        }
       }
     }
   }
@@ -43,21 +39,15 @@
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header">Register</div>
+        <div class="card-header">Login</div>
         <div class="card-body">
           <?php if ($error != null): ?>
             <p class="text-danger">
               <?= $error ?>
             </p>
           <?php endif ?> 
-          <form method="POST" action="register.php">
+          <form method="POST" action="login.php">
             <div class="mb-3 row">
-              <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
-
-              <div class="col-md-6">
-                <input id="name" type="text" class="form-control" name="name" autocomplete="name" autofocus>
-              </div>
-            </div>
 
             <div class="mb-3 row">
               <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
